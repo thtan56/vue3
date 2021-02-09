5ba0d48ae91683f83357471ef59231d85138a625
huat <thtan56@gmail.com>
2021-02-09 22:38:51 +0800

    2nd commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>Example Component, {{ greeting }}</div>
+    <div>Foo: {{ foo }}</div>
+</template>
+
+<script>
+import {ref} from 'vue'
+export default {
+    props: ['foo'],
+    setup() {
+       const greeting= ref('Component setup');
+       return {
+           greeting
+       }
+    }
+}
+</script>
